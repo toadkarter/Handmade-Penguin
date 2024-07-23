b6ea934d5e61993924f3a277d86c6fe0ce20ea47
toadkarter <vrakhmanin1@gmail.com>
2024-07-23 22:21:23 +0100

    Adds basic window opening functionality.

--- handmade/code/sdl_handmade.cpp
+++ handmade/code/sdl_handmade.cpp
@@ -1,7 +1,75 @@
 #include <SDL2/SDL.h>
 
+bool HandleEvent(SDL_Event* Event)
+{
+    bool ShouldQuit = false;
+    uint32_t EventType = Event->type;
+
+    switch (EventType)
+    {
+        case SDL_QUIT:
+        {
+            ShouldQuit = true;
+            break;
+        }
+
+        case SDL_WINDOWEVENT:
+        {
+            Uint8 WindowEvent = Event->window.event;
+            switch (WindowEvent)
+            {
+                case SDL_WINDOWEVENT_RESIZED:
+                {
+                    printf("Window has been resized (%d, %d)\n", Event->window.data1, Event->window.data2);
+                    break;
+                }
+
+                default:
+                {
+                    break;
+                }
+            }
+            break;
+        }
+
+        default:
+        {
+            break;
+        }
+    }
+
+    return ShouldQuit;
+}
+
 int main(int argc, char **argv)
 {
-    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Handmade Hero", "This is handmade hero", 0);
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        // TODO: SDL Initialisation failed.
+        return 1;
+    }
+
+    SDL_Window* Window;
+    Window = SDL_CreateWindow("Handmade Hero",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              640,
+                              480,
+                              SDL_WINDOW_RESIZABLE);
+
+
+    for (;;)
+    {
+        SDL_Event Event;
+        SDL_WaitEvent(&Event);
+        if (HandleEvent(&Event))
+        {
+            break;
+        }
+
+    }
+
     return 0;
+
 }
