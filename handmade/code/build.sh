f787a35967fd636d7813c4b1e67a5f62cd820143
toadkarter <vrakhmanin1@gmail.com>
2024-07-23 19:02:29 +0100

    Adds message box to test SDL integration.

--- handmade/code/build.sh
+++ handmade/code/build.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ../../build
 pushd ../../build
-c++ ../handmade/code/sdl_handmade.cpp -o handmadehero -g
+c++ ../handmade/code/sdl_handmade.cpp -o handmadehero -g `sdl2-config --cflags --libs`
--- handmade/code/sdl_handmade.cpp
+++ handmade/code/sdl_handmade.cpp
@@ -1,4 +1,7 @@
+#include <SDL2/SDL.h>
+
 int main(int argc, char **argv)
 {
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Handmade Hero", "This is handmade hero", 0);
     return 0;
 }
